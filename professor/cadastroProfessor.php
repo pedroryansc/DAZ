16979120d2f32aaa8997e3cd05a2dcb5057accf6
Jaspeer43 <107075951+Jaspeer43@users.noreply.github.com>
2022-08-24 17:37:23 -0300

    Desenvolvimento do CSS (Tela de cadastro e login do professor)

--- professor/cadastroProfessor.php
+++ professor/cadastroProfessor.php
@@ -16,11 +16,226 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro (Professor) - DAZ</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+        .input1{
+            position: absolute;
+width: 465.8px;
+height: 50px;
+left: 106px;
+top: 512px;
+
+background: #D9D9D9;
+border-radius: 10px;
+border-color: #D9D9D9;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 200;
+font-size: 20px;
+line-height: 23px;
+        }
+        .input2{
+            position: absolute;
+width: 465.8px;
+height: 50px;
+left: 106px;
+top: 584px;
+
+background: #D9D9D9;
+border-radius: 10px;
+border-color: #D9D9D9; 
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 200;
+font-size: 20px;
+line-height: 23px;
+        }
+        .input3{
+            position: absolute;
+width: 459.24px;
+height: 50px;
+left: 106px;
+top: 656px;
+
+background: #D9D9D9;
+border-radius: 10px;
+border-color: #D9D9D9;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 200;
+font-size: 20px;
+line-height: 23px;
+
+        }
+        .input4{
+            position: absolute;
+width: 459.24px;
+height: 50px;
+left: 106px;
+top: 728px;
+
+background: #D9D9D9;
+border-radius: 10px;
+border-color: #D9D9D9;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 200;
+font-size: 20px;
+line-height: 23px;
+        }
+        .input5{
+            position: absolute;
+width: 459.24px;
+height: 50px;
+left: 106px;
+top: 800px;
+
+background: #D9D9D9;
+border-radius: 10px;
+border-color: #D9D9D9;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 200;
+font-size: 20px;
+line-height: 23px;
+        }
+        .input6{
+            position: absolute;
+width: 459.24px;
+height: 50px;
+left: 106px;
+top: 800px;
+
+background: #D9D9D9;
+border-radius: 10px;
+border-color: #D9D9D9;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 200;
+font-size: 20px;
+line-height: 23px
+        }
+        .input7{
+            position: absolute;
+width: 459.24px;
+height: 50px;
+left: 106px;
+top: 872px;
+
+background: #D9D9D9;
+border-radius: 10px;
+border-color: #D9D9D9;
+
+font-family: 'Roboto';
+font-style: normal;
+font-weight: 200;
+font-size: 20px;
+line-height: 23px
+
+        }
+        button{
+            position: absolute;
+            width: 280px;
+            height: 50px;
+            left: 196px;
+            top: 944px;
+
+            background: linear-gradient(267.77deg, #BDBE6E 7.05%, #F3BD1D 49.14%, #BAD9C9 89.52%);
+            border-radius: 10px;
+            border-color: #D9D9D9;
+            border-color: #FFFFFE;
+
+            font-family: 'Roboto';
+font-style: normal;
+font-weight: 200;
+font-size: 20px;
+line-height: 23px;
+        }
+        body{
+            background: linear-gradient(270deg, #BDBE6E 6.17%, #F3BD1D 48.36%, #BAD9C9 87.94%);
+            
+        }
+        .branco{
+            position: absolute;
+            width: 668px;
+            height: 1024px;
+            left: 0px;
+            top: 0px;
+
+            background: #FFFFFE;
+        }
+        .img1{
+            position: absolute;
+width: 1024px;
+height: 504px;
+left: 1172px;
+top: 0px;
+
+transform: rotate(90deg);
+        }
+        .img2{
+            position: absolute;
+width: 1024px;
+height: 38px;
+left: 1308px;
+top: 0px;
+
+
+transform: rotate(90deg);
+        }
+        .voltar{
+            box-sizing: border-box;
+
+position: absolute;
+width: 55px;
+height: 0px;
+left: 85px;
+top: 51px;
+
+border: 7px solid #BDBE6E;
+transform: rotate(180deg);
+        }
+        .volta{
+            box-sizing: border-box;
+
+position: absolute;
+width: 55px;
+height: 0px;
+left: 85px;
+top: 51px;
+
+border: 7px solid #BDBE6E;
+transform: rotate(180deg);
+
+        }
+        .perfi{
+            position: absolute;
+width: 338px;
+height: 338px;
+left: 170px;
+top: 97px;
+
+background: conic-gradient(from 180deg at 50% 50%, #BAD9C9 0deg, #BDBE6E 15deg, #F3BD1D 200.63deg, #BAD9C9 360deg);
+        }
+
+    </style>
 </head>
-<body>
-    <a href="<?php if($acao == "editar") echo "principalProfessor.php?id=".$id; else echo "loginProfessor.php"; ?>">Voltar</a><br>
+<body >
+  
+<div class="branco">
+    <div class="volta"></div>
+    <a href="<?php if($acao == "editar") echo "principalProfessor.php?id=".$id; else echo "loginProfessor.php"; ?>" class="voltar"><img src="img\flecha.svg" alt="" srcset=""></a><br>
     <br><br>
-    <form action="../control/ctrl_professor.php?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
+    <div class="perfil"></div>  
+   
+        <form action="../control/ctrl_professor.php?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
         <!--
             (Pesquisar pela função de criar uma pasta automaticamente. A pasta serviria para armazenar as imagens do usuário)
 
@@ -28,24 +243,27 @@
             <input type="file" name="imagem"><br>
             <br>
         -->
-        <input type="text" name="nome" placeholder="Nome" value="<?php if($acao == "editar") echo $vetor[0]["nome"]; ?>"><br>
+        
+        <input type="text" name="nome" placeholder="Nome" value="<?php if($acao == "editar") echo $vetor[0]["nome"]; ?>" class="input1"><br>
         <br>
         <input type="text" name="sobrenome" placeholder="Sobrenome"
-        value="<?php if($acao == "editar") echo $vetor[0]["sobrenome"]; ?>"><br>
+        value="<?php if($acao == "editar") echo $vetor[0]["sobrenome"]; ?>"class="input2"><br>
         <br>
         <input type="text" name="areaAtuacao" placeholder="Área de atuação"
-        value="<?php if($acao == "editar") echo $vetor[0]["areaAtuacao"]; ?>"><br>
+        value="<?php if($acao == "editar") echo $vetor[0]["areaAtuacao"]; ?>"class="input3"><br>
         <br>
         <input type="text" name="formacao" placeholder="Formação"
-        value="<?php if($acao == "editar") echo $vetor[0]["formacao"]; ?>"><br>
+        value="<?php if($acao == "editar") echo $vetor[0]["formacao"]; ?>"class="input4"><br>
         <br>
-        <input type="text" name="email" placeholder="E-mail" value="<?php if($acao == "editar") echo $vetor[0]["email"]; ?>"><br>
+        <input type="text" name="email" placeholder="E-mail" value="<?php if($acao == "editar") echo $vetor[0]["email"]; ?>"class="input5"><br>
         <br>
-        <input type="password" name="senha" placeholder="Senha" value="<?php if($acao == "editar") echo $vetor[0]["senha"]; ?>"><br>
+        <input type="password" name="senha" placeholder="Senha" value="<?php if($acao == "editar") echo $vetor[0]["senha"]; ?>"class="input6"><br>
         <br>
-        <input type="password" name="confirmarSenha" placeholder="Confirme sua senha"><br>
+        <input type="password" name="confirmarSenha" placeholder="Confirme sua senha" class="input7"><br>
         <br>
         <button type="submit" name="acao" value="salvar">Salvar</button>
-    </form>
+    </form></div>
+    <img src="../img/Frame3.svg" class="img1">
+    <img src="../img/PLATAFORMA PARA ALFABETIZAÇÃO DE JOVENS E ADULTOS.svg" alt="" srcset="" class="img2">
 </body>
 </html>
--- professor/loginProfessor.php
+++ professor/loginProfessor.php
@@ -9,16 +9,103 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login (Professor) - DAZ</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap');
+      body{
+       
+width: 1028px;
+height: 1472px;
+left: 850x;
+top: 1028px;
+
+background: linear-gradient(270deg, #BDBE6E 6.17%, #F3BD1D 48.36%, #BAD9C9 87.94%);
+
+      } 
+      .input1{
+        position: absolute;
+width: 578px;
+height: 34px;
+left: 850px;
+top: 610px;
+
+background: #EFF0F3;
+border-radius: 10px;
+border-color: #EFF0F3;
+
+font-family: 'Nunito';
+font-style: normal;
+font-weight: 300;
+font-size: 20px;
+line-height: 27px;
+      }
+      .input2{
+        position: absolute;
+width: 578px;
+height: 39px;
+left: 850px;
+top: 680px;
+
+background: #EFF0F3;
+border-radius: 10px;
+border-color: #EFF0F3;
+
+font-family: 'Nunito';
+font-style: normal;
+font-weight: 300;
+font-size: 20px;
+line-height: 27px;
+      } 
+      button{
+        position: absolute;
+width: 280px;
+height: 50px;
+left: 850px;
+top: 809px;
+
+background: linear-gradient(89.94deg, #EDBF2B 0.05%, #BDBE6E 97.46%);
+border-radius: 10px;
+border-color: #EFF0F3;
+      }
+      img{
+        position: absolute;
+width: 516.87px;
+height: 253.45px;
+left: 850px;
+top: 190px;
+      }
+      p{
+        position: absolute;
+width: 303px;
+height: 20px;
+left: 850px;
+top: 748px;
+
+font-family: 'Nunito';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 27px;
+
+color: #272343;
+      }
+      a{
+        color: black;
+      }
+    </style>
+
 </head>
 <body>
     <a href="../inicial.html">Voltar</a><br>
     <br><br>
+    <center>
+    <img src="../img/fulllogo.png">
     <form action="../control/ctrl_professor.php" method="post">
-        <input type="text" name="email" placeholder="E-mail"><br>
+        <input type="text" name="email" placeholder="E-mail" class="input1"><br>
         <br>
-        <input type="password" name="senha" placeholder="Senha"><br>
+        <input type="password" name="senha" placeholder="Senha" class="input2"><br>
         <p>Não tem uma conta? <a href="cadastroProfessor.php">Clique aqui</a></p>
         <button type="submit" name="acao" value="login">Entrar</button>
     </form>
+    </center>
 </body>
 </html>
