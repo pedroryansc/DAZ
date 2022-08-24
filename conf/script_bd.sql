6e51cbab3add6465ce1ede5896ce922aa7265ee0
pedroryansc <pedroryangamer@gmail.com>
2022-08-23 22:51:49 -0300

    Atualizações no Script do banco de dados

--- conf/script_bd.sql
+++ conf/script_bd.sql
@@ -15,22 +15,23 @@ CREATE TABLE turma(
     nome varchar(45),
     instituicao varchar(250),
     mediaGeral decimal(6,1),
-    professor_idprofessor INT
+    professor_idprofessor INT NOT NULL,
     FOREIGN KEY (professor_idprofessor) references professor (idprofessor));
 
 CREATE TABLE conjuntoQuestoes(
-    idconjuntoQuestoes INT,
+    idconjuntoQuestoes INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nome varchar(45),
     tags varchar(250),
-    professor_idprofessor INT,
+    imagem blob,
+    professor_idprofessor INT NOT NULL,
     FOREIGN KEY (professor_idprofessor) references professor (idprofessor));
 
 CREATE TABLE conjuntoQuestoes_has_turma(
     conjuntoQuestoes_idconjuntoQuestoes INT NOT NULL,
-    turma_idturma INT,
+    turma_idturma INT NOT NULL,
+    PRIMARY KEY (conjuntoQuestoes_idconjuntoQuestoes, turma_idturma),
     FOREIGN KEY (conjuntoQuestoes_idconjuntoQuestoes) references conjuntoQuestoes (idconjuntoQuestoes),
-    FOREIGN KEY (turma_idturma) references turma (idturma);
-);
+    FOREIGN KEY (turma_idturma) references turma (idturma));
 
 CREATE TABLE aluno(
     idaluno INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -40,24 +41,27 @@ CREATE TABLE aluno(
     numQuestResp INT,
     numAcertos INT,
     media decimal(6,1),
-    professor_idprofessor INT,
-    turma_idturma INT,
+    fotoPerfil blob,
+    professor_idprofessor INT NOT NULL,
+    turma_idturma INT NOT NULL,
     FOREIGN KEY (professor_idprofessor) references professor (idprofessor),
     FOREIGN KEY (turma_idturma) references turma (idturma));
 
 CREATE TABLE questao(
     idquestao INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    tipo varchar(45),
+    tipo INT,
     enunciado varchar(250),
     midia blob,
     tags varchar(250),
-    professor_idprofessor INT,
-    conjuntoQuestoes_idconjuntoQuestoes INT,
+    professor_idprofessor INT NOT NULL,
+    conjuntoQuestoes_idconjuntoQuestoes INT NOT NULL,
     FOREIGN KEY (professor_idprofessor) references professor (idprofessor),
     FOREIGN KEY (conjuntoQuestoes_idconjuntoQuestoes) references conjuntoQuestoes (idconjuntoQuestoes));
 
 CREATE TABLE questao_has_aluno(
     questao_idquestao INT NOT NULL,
     aluno_idaluno INT NOT NULL,
+    resposta varchar(250),
+    PRIMARY KEY (questao_idquestao, aluno_idaluno),
     FOREIGN KEY (questao_idquestao) references questao (idquestao),
     FOREIGN KEY (aluno_idaluno) references aluno (idaluno));
